791e8c0d8536e8bb20889a3d8df7fcd2a5f58ac5
mcn16 <miranda.c.negro@gmail.com>
2025-01-17 20:36:05 -0500

    update

--- tiktok.bat
+++ tiktok.bat
@@ -1,6 +1,14 @@
 @echo off
 color 0A
-
+:banner
+ echo
+ echo  _____ ___ _    __________________  __ __
+ echo / ___//   | |  / / ____/_  __/ __ \/ //_/
+ echo \__ \/ /| | | / / __/   / / / / / / ,<   
+ echo___/ / ___ | |/ / /___  / / / /_/ / /| |  
+echo/____/_/  |_|___/_____/ /_/  \____/_/ |_|  
+echo                                           
+echo                                                                                   
 cd "C:\Users\miran\TikTok-Video-Downloader-using-Python-and-yt-dlp"
 if %ERRORLEVEL% NEQ 0 (
     echo Failed to change directory to TikTok-Video-Downloader-using-Python-and-yt-dlp
@@ -25,7 +33,7 @@ echo 3. Exit
 set /p choice=Enter your choice (1, 2, or 3): 
 
 if "%choice%"=="1" goto run_script
-if "%choice%"=="2" goto add_url
+if "%choice%"=="2" goto add_urls
 if "%choice%"=="3" goto end
 
 
@@ -42,11 +50,13 @@ if %ERRORLEVEL% NEQ 0 (
 pause
 goto end
 
-:add_url
-set /p URL=Enter the URL: 
+:add_urls
+:input_url
+set /p URL=Enter the URL (type 'menu' to return to the main menu): 
+if "%URL%"=="menu" goto menu
 set URLS=%URLS% %URL%
-echo URLs: %URLS%
-goto menu
+echo URLs added so far: %URLS%
+goto input_url
 
 
 
